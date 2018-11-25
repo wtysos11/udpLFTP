3dc873c6dddee6c58535bfba27eeeca3d6f281e9
wtysos11 <wtysos11@163.com>
2018-11-25 20:29:09 +0800

    实验1改：queue的非阻塞与timeout

--- server.py
+++ server.py
@@ -1,20 +1,28 @@
 from multiprocessing import Queue
-import os,socket,threading,time
+import os,socket,threading,time,queue
 
 def receive_server(q):
     base = 0
     while True:
-        data = q.get()
-        print(data)
+        try:
+            data = q.get(timeout = 2.0)
+            print(data)
+        except queue.Empty:
+            print("Empty")
         t = time.time()
         if t - base >10:
             base = t
             print(base)
 
 def output_server(q,s):
+    base1 = 0
     while True:
         data = s.recvfrom(1024)
         q.put(data)
+        t = time.time()
+        if t - base1 > 10:
+            print("output")
+            base1 = t
 
 s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 s.bind(('localhost',9999))
