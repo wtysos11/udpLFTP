b3ecfcf79b58acf7789059a7d8b89ba39a3a1288
wtysos11 <wtysos11@163.com>
2018-11-25 19:32:16 +0800

    实验1：确定了Queue.get是阻塞的

--- server.py
+++ server.py
@@ -1,12 +1,25 @@
-import socket
+from multiprocessing import Queue
+import os,socket,threading,time
+
+def receive_server(q):
+    base = 0
+    while True:
+        data = q.get()
+        print(data)
+        t = time.time()
+        if t - base >10:
+            base = t
+            print(base)
+
+def output_server(q,s):
+    while True:
+        data = s.recvfrom(1024)
+        q.put(data)
 
 s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-print("socket start to work on 9999 port on localhost")
 s.bind(('localhost',9999))
-print("Bind successful")
-
-while True:
-    data,addr = s.recvfrom(4096)
-    print(data)
-    print(addr)
-    s.sendto(b'Hello,%s!'%data,addr)
+q = Queue()
+out_thread = threading.Thread(target = output_server,args = (q,s,))
+in_thread = threading.Thread(target = receive_server, args = (q,))
+out_thread.start()
+in_thread.start()
