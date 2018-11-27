02c866be4fd531b43ac9169809813a41ad61e8f6
wtysos11 <wtysos11@163.com>
2018-11-27 19:22:42 +0800

    implement GBN. Pass multiple packet at one time test.

--- client.py
+++ client.py
@@ -1,29 +1,35 @@
 import socket
 from packetHead import packetHead,generateBitFromDict 
 
+clientReceivePackMax = 1024 #客户端接受数据包的长度最大为1024bytes
+
 s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 s.bind(('127.0.0.1',9990))
 s.sendto(generateBitFromDict({"optLength":8,"Options":b"test.txt"}),('127.0.0.1',9999))
-ackValue = 1
-hopeSEQvalue = 1
+expectedSeqValue = 0
 drop = 0
+'''
+GBN接受方逻辑
+不断收包
+    如果收到的包符合expectedSeqValue，输出，并expectedSeqValue += 1
+返回ACK expectedSeqValue
+'''
 while True:
-    data,addr = s.recvfrom(1024)
+    data,addr = s.recvfrom(clientReceivePackMax)
     packet = packetHead(data)
-    print(packet.dict["Data"],addr)
+    print("receive ",packet.dict["Data"])
+    '''
     drop = int(not bool(drop))
     if drop == 1:
         print("Drop packet")
         continue
-
-    if packet.dict["FIN"] == b'1':
+    '''
+    if packet.dict["FIN"] == b'1':#如果收到FIN包，则退出
         print("receive eof, client over.")
         break
-    elif packet.dict["SEQvalue"] != hopeSEQvalue:
-        print("receive not receive hope SEQvalue",packet.dict["SEQvalue"])
-        print("Hope for ",hopeSEQvalue)
-        s.sendto(generateBitFromDict({"ACKvalue":int(not bool(ackValue)),"ACK":b'1'}),('127.0.0.1',int(addr[1])-1))
-    else:
-        s.sendto(generateBitFromDict({"ACKvalue":ackValue,"ACK":b'1'}),('127.0.0.1',int(addr[1])-1))
-        ackValue = int(not bool(ackValue))
-        hopeSEQvalue = ackValue
+    elif packet.dict["SEQvalue"] == expectedSeqValue:
+        print(packet.dict["Data"])
+        s.sendto(generateBitFromDict({"ACKvalue":expectedSeqValue,"ACK":b'1'}),('127.0.0.1',int(addr[1])-1))
+        expectedSeqValue += 1
+    else:#收到了不对的包
+        s.sendto(generateBitFromDict({"ACKvalue":expectedSeqValue-1,"ACK":b'1'}),('127.0.0.1',int(addr[1])-1))
--- server.py
+++ server.py
@@ -1,9 +1,10 @@
-import socket,queue,threading
+import socket,queue,threading,time
 from packetHead import packetHead,generateBitFromDict
 
 #经常使用的常量值
 GBNWindowMax = 1000 #GBN窗口大小，意味最多等待1000个未确认的包
 senderTimeoutValue = 1.0 #下载时发送端等待超时为1.0s
+senderPacketDataSize = 50 #从文件中读取的数据的大小，发送包中数据的大小。
 
 # queue类q用来传递ack的值
 def receiver(port,q):
@@ -24,39 +25,74 @@ def receiver(port,q):
     receiverSocket.close()
     print("receiver close")
 
+'''
+GBN发送方逻辑
+尝试从文件中读取数据
+    是否能够进行发送
+        如果当前发送的包没有超过数量或是阻塞控制上限，则打包、缓存，并进行发送。
+        如果已经满了，则置sendValuable = False
+    发送完之后，检查是否接受到ACK并判断超时。
+    对于接受到的ACK，baseSEQ进行更新。
+    如果baseSEQ = nextseqnum，则解除置位,sendValuable = True
+'''
 def sender(port,q,fileName,addr):
+    print("Enter sender with filename",fileName)
     senderSocket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
     senderSocket.bind(('127.0.0.1',port))
     f = open(filename,"rb")
-    SEQvalue = 1
-    hopeACKvalue = 1
-    baseSYN = 0
+    #待确认的包的数量nextseqnum - baseSEQ <= GBNWindowMax
+    baseSEQ = 0
     nextseqnum = 0
-    while True:
-        data = f.read(50)
-        print("sender read",data)
-        if data == b'':
-            print("File read end.")
-            senderSocket.sendto(generateBitFromDict({"optLength":3,"Options":b"end","FIN":b'1'}),('127.0.0.1',port-1))
-            senderSocket.sendto(generateBitFromDict({"optLength":3,"Options":b"eof","FIN":b'1'}),addr)
-            break
-        
-        senderSocket.sendto(generateBitFromDict({"SEQvalue":SEQvalue,"SYN":b'1',"Data":data}),addr)
-        receiveCurrentACK = False
-        while not receiveCurrentACK:
+    GBNtimer = 0
+    GBNcache = {}
+    sendValueable = True
+    senderClose = False
+    while not senderClose:
+        while sendValueable:#如果可以读入数据
+            data = f.read(senderPacketDataSize)
+            print("sender read file with data: ",data)
+            data = f.read(senderPacketDataSize)
+            if data == b'':#文件读入完毕
+                print("File read end.")
+                sendValueable = False
+                senderClose = True
+            #缓存并发送
+            if nextseqnum == baseSEQ:
+                GBNtimer = time.time()
+            GBNcache[nextseqnum] = generateBitFromDict({"SEQvalue":nextseqnum,"Data":data})
+            senderSocket.sendto(GBNcache[nextseqnum],addr)
+            print("Sender send",data)
+            nextseqnum += 1
+            if nextseqnum - baseSEQ >=GBNWindowMax:
+                sendValueable = False
+
+        #等待接收ACK
+        receiveACK = False
+        while not receiveACK:
             try:
                 ack = q.get(timeout = senderTimeoutValue)
-                if ack == hopeACKvalue:
-                    receiveCurrentACK = True
-                    hopeACKvalue = int(not bool(hopeACKvalue))
-                    SEQvalue = hopeACKvalue
-                else: #收到了不正确的ACK
-                    print("Receive wrong ack",ack," and hope for ack",hopeACKvalue)
-                    senderSocket.sendto(generateBitFromDict({"SEQvalue":SEQvalue,"SYN":b'1',"Data":data}),addr)
+                if ack >= baseSEQ:
+                    baseSEQ = ack+1
+                    receiveACK = True #收到ACK，脱离超时循环
+                    GBNtimer = time.time()#更新计时器
+                    if baseSEQ == nextseqnum:#前一阶段发送完毕
+                        sendValueable = True
+                        break
+                    
+                currentTime = time.time()
+                if currentTime - GBNtimer > senderTimeoutValue:
+                    GBNtimer = time.time()#更新计时器
+                    for i in range(baseSEQ,nextseqnum):
+                        senderSocket.sendto(GBNcache[i],addr)
             except queue.Empty: #超时，发包
                 print("Time out")
-                senderSocket.sendto(generateBitFromDict({"SEQvalue":SEQvalue,"SYN":b'1',"Data":data}),addr)
+                GBNtimer = time.time()#更新计时器
+                for i in range(baseSEQ,nextseqnum):
+                    senderSocket.sendto(GBNcache[i],addr)
         print("sender receive ack")
+    #关闭接受端与客户端
+    senderSocket.sendto(generateBitFromDict({"optLength":3,"Options":b"eof","FIN":b'1'}),addr)
+    senderSocket.sendto(generateBitFromDict({"optLength":3,"Options":b"end","FIN":b'1'}),('127.0.0.1',port-1))
     senderSocket.close()
     f.close()
     print("sender closes")
@@ -72,6 +108,7 @@ while serverConnected:
     print("Main thread receive data",data)
     packet = packetHead(data)
     filename = packet.dict["Options"].decode("utf-8")
+    print("Main thread receive filename: ",filename)
     transferQueue = queue.Queue()
     rec_thread = threading.Thread(target = receiver,args = (appPortNum,transferQueue,))
     send_thread = threading.Thread(target = sender,args = (appPortNum+1,transferQueue,filename,addr,))
