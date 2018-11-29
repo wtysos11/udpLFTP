49d2fd1962126ddb9f1f52cc4a80223c494e759d
wtysos11 <wtysos11@163.com>
2018-11-29 14:42:25 +0800

    very simple c-s model. Test NAT transport.

--- testClient.py
+++ testClient.py
@@ -3,8 +3,13 @@ from rdtPacketTransfer import *
 from packetHead import *
 
 s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-host = '123.207.228.157'
+#host = '123.207.228.157'
+host = '127.0.0.1'
 portNum = 9999
 sAddr = (host,portNum)
 s.bind(('',10000))#if add this, then it can't send to remote server
-s.sendto(b'Hello',sAddr)
+dataList = [b'He',b'el',b'lo']
+for i in dataList:
+    s.sendto(i,(host,portNum))
+    data,addr = s.recvfrom(1024)
+    print(data,addr)
--- testServer.py
+++ testServer.py
@@ -4,12 +4,8 @@ from packetHead import *
 import socket,queue,random
 
 s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-s.bind(('127.0.0.1',9999))
+s.bind(('',9999))
 while True:
     data,addr = s.recvfrom(1024)
-    packet = packetHead(data)
-    if random.random()>0.5:
-        print("Drop packet")
-        continue
-    print("Receive with seq value ",packet.dict["SEQvalue"])
-    s.sendto(generateBitFromDict({"ACK":b'1',"ACKvalue":packet.dict["SEQvalue"]}),addr)
+    print("Receive data")
+    s.sendto(b'Hello',addr)
